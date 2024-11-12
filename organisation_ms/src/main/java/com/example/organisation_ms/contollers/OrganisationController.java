41e173072b7003bb3f2822ac29e4da29eb48ffd5
YVSER <108676377+YVSER@users.noreply.github.com>
2024-11-12 19:35:59 +0100

    GG3

--- organisation_ms/src/main/java/com/example/organisation_ms/contollers/OrganisationController.java
+++ organisation_ms/src/main/java/com/example/organisation_ms/contollers/OrganisationController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-
+@RequestMapping("/organisation")
 public class OrganisationController {
     @Autowired
     private OrganisationService organisationService;
